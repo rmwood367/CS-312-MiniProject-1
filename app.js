a30cd213e7a689e4e54d3f83f877d277b70db13e
rmwood367 <rmw367@nau.edu>
2024-09-16 22:03:31 -0700

    Added Files Via Upload

---
+++ app.js
@@ -0,0 +1,75 @@
+const express = require ('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// keep the blog posts in the session 
+let posts = [];
+
+// Route to render index.ejs
+app.get('/', (req, res) => 
+{
+  res.render('index', { blogposts: posts });
+});
+
+// Route to the create a post page
+app.get('/posts', (req, res) => 
+{
+  res.render('posts');
+});
+
+
+app.post('/posts', (req, res) => 
+{
+  const post = {
+    //post creation format
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author,
+    createdAt: new Date()
+  };
+  posts.push(post);
+  res.redirect('/');
+});
+
+// Route to open the edit page
+app.get('/blogposts/edit/:id', (req, res) =>
+ {
+  const postId = req.params.id;
+  const post = posts[postId];
+  res.render('edit', { post: post, id: postId });
+}
+)
+;
+
+// Route to handle the updates
+app.post('/posts/edit/:id', (req, res) => 
+{
+  const postId = req.params.id;
+  posts[postId] = {
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author,
+  };
+  res.redirect('/');
+}
+);
+
+// Route to handle post deletion
+app.post('/posts/delete/:id', (req, res) => 
+{
+  const postId = req.params.id;
+  posts.splice(postId, 1);
+  res.redirect('/');
+}
+);
+
+
+// initialize the server
+app.listen(3000, () => 
+{
+  console.log('website initalized at http://localhost:3000');
+});
